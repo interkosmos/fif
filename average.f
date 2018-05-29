bcc614a0908b446beb5960a5edc1b6e0660bc62f
interkosmos <pengel@hs-nb.de>
2018-05-29 15:28:18 +0200

    Updated example.

--- average.f
+++ average.f
@@ -1,14 +1,17 @@
-      DIMENSION A(10)
- 1    FORMAT (I3/(F6.3))
-      READ 1 N, (A(J), J = 1,N)
+C     PROGRAM FOR FINDING THE AVERAGE OF A
+C    X    SET OF NUMBERS
+      DIMENSION A(999)
+    1 FORMAT (I3/(F6.3))
+      READ 1 N, (A(J), J = 1, N)
       SUM = 0.0
-      DO 5 I = 1, N
-      IF (A(I)) 4, 2, 2
- 2    SUM = SUM + A(I)
-      PRINT 1 A(I)
+      DO 6 I = 1, N
+      IF (A(I)) 5, 2, 2
+    2 SUM = SUM + A(I)
+    3 FORMAT (F6.3)
+      PRINT 3 A(I)
       CONTINUE
- 3    FORMAT (21HNegative number found)
- 4    PRINT 3
- 5    CONTINUE
- 6    FORMAT (3HHi F6.3)
-      PRINT 6 SUM / N
+    4 FORMAT (21HNEGATIVE NUMBER FOUND)
+    5 PRINT 4
+    6 CONTINUE
+    7 FORMAT (9HAVERAGE: F6.3)
+      PRINT 7 SUM / N
